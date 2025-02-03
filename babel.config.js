9fa20ebee0e5c206c02998387d68ebedf3c95e18
eunung <pandamon24@naver.com>
2025-02-03 16:27:46 +0900

    hotfix: GHA - nextJS + babel config 충돌 오류 처리

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    presets: [
-        ['@babel/preset-env', {targets: {node: 'current'}}],
-        ["@babel/preset-react", {
-            runtime: "automatic", // React 자동 import 설정
-        }],
-        '@babel/preset-typescript',
-    ],
-};
